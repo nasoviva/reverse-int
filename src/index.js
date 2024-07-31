e79ce1ee72490d68599f1c4fc3b8f6e8435b4ac0
nasoviva <nasoviva@gmail.com>
2024-07-31 13:44:10 -0300

    feat: function are ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let res = 0;
+    let str = n.toString();
+    let i = 1;
+    let newStr = '';
+    while (i <= str.length) {
+        newStr = newStr + str[str.length - i]
+        i++;
+    }
+    res = parseInt(newStr);
+  return res;
 }
